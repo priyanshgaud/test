9671c281f4955365760d5b474003ee28d254960c
priyanshgaud <priyanshgaud71@gmail.com>
2025-07-08 15:05:36 +0530

    Add files via upload

---
+++ python progrmas/p5.py
@@ -0,0 +1,28 @@
+# n=int(input("Enter a number: "))
+# if (n>=2000):
+#     b=2000
+#     print("%10 is discount")
+#     p= (n / b) * 200
+#     print(" is discount",p)
+#     c= n-(n/10)
+#     print(" is discount",c) 
+# else:
+#     print("discount %0",n)
+# n = int(input("Enter a number: "))
+# b = (input("Enter + - / *: "))
+# c = int(input("Enter noumber: "))
+# if b == "+":
+#     print("Addition is", n + c)
+# elif b == "-":
+#     print("Subtraction is", n - c)
+# elif b == "*":
+#     print("Multiplication is", n * c)
+# elif b == "/":  
+#     if c != 0:
+#         print("Division is", n / c)
+#     else:
+#         print("Cannot divide by zero")
+# else:
+#     print("Invalid operation")
+# print("End of program")
+
